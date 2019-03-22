f4f6b9ef057d0b68588d340ac4a922f3854915d3
chebozh <ch_bozhinov@hotmail.co.uk>
2019-03-22 19:55:59 +0100

    change db uri

--- BGInfoPortal/config.py
+++ BGInfoPortal/config.py
@@ -3,4 +3,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///bginfoportal.db'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
