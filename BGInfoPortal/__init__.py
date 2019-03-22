110e38d42ef91e36b00b41bdd86f94a538b93d52
chebozh <ch_bozhinov@hotmail.co.uk>
2019-03-22 19:46:25 +0100

    added flask-heroku settings

--- BGInfoPortal/__init__.py
+++ BGInfoPortal/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_heroku import Heroku
+
 from BGInfoPortal.config import Config
 
 db = SQLAlchemy()
@@ -7,6 +9,8 @@ db = SQLAlchemy()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    heroku = Heroku()
+    heroku.init_app(app)
     app.config.from_object(config_class)
     db.init_app(app)
 
