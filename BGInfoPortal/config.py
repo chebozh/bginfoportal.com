068a155fcc4ce96c778519fd1b7a257f45e1b9be
chebozh <ch_bozhinov@hotmail.co.uk>
2019-03-22 23:36:53 +0100

    changed db config to hk env var

--- BGInfoPortal/config.py
+++ BGInfoPortal/config.py
@@ -3,5 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DB_URI')
-    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # local
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DB_URI')
+    # hk
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
